25014a653406b8e334634daf03d16abf886fcdef
AKG <weiweijiangcn@gmail.com>
2017-08-16 16:15:24 +0900

    update format

--- src/MyChart.py
+++ src/MyChart.py
@@ -3,7 +3,7 @@ import numpy
 from PyQt4 import QtCore, QtGui, Qwt5
 
 class CurveChart(object):
-	def __init__(self, ):
-		super(self, CurveChart).__init__()
+    def __init__(self, ):
+        super(self, CurveChart).__init__()
 
 
